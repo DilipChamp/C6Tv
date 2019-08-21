14f2972417a4162df301e9987823248ebfbdb755
Dileep <dileep@mobac.co.in>
2019-08-21 20:57:25 +0530

    commit

--- app/src/main/java/com/dilip/c6tv/LiveTV.java
+++ app/src/main/java/com/dilip/c6tv/LiveTV.java
@@ -1,51 +1,25 @@
 package com.dilip.c6tv;
 
-import android.app.ProgressDialog;
-import android.media.MediaPlayer;
-import android.net.Uri;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.MediaController;
-import android.widget.VideoView;
+import android.webkit.WebView;
+
+
+
+
 
 public class LiveTV extends AppCompatActivity {
-ProgressDialog progressDialog;
-VideoView videoView;
-String VideoUrl="https://www.androidbegin.com/tutorial/AndroidCommercial.3gp";
+
+    WebView views;
+    String html="<iframe src='http://www.ssh101.com/securelive/index.php?id=c6tv' width=1920 height=1080></iframe>";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_tv);
-        videoView=(VideoView)findViewById(R.id.VideoView);
-        progressDialog=new ProgressDialog(LiveTV.this);
-        progressDialog.setTitle("Live Streaming Video");
-        progressDialog.setMessage("under construction coming soon....");
-        progressDialog.setIndeterminate(false);
-        progressDialog.setCancelable(true);
-        progressDialog.show();
-        try
-        {
-            MediaController mediaController=new MediaController(LiveTV.this);
-            mediaController.setAnchorView(videoView);
-            Uri video=Uri.parse(VideoUrl);
-            videoView.setMediaController(mediaController);
-            videoView.setVideoURI(video);
-        }
-        catch (Exception e)
-        {
-            Log.e("Error",e.getMessage());
-            e.printStackTrace();
-        }
-        videoView.requestFocus();
-
-        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                progressDialog.dismiss();
-                videoView.start();
-            }
-        });
+        views= (WebView) findViewById(R.id.webViews);
+        views.getSettings().setJavaScriptEnabled(true);
+        views.loadData(html, "text/html", null);
 
     }
 
